8b66a42c12db983fcc211d2f820fccc22d35f0dd
Rafael <81027973+Rafael763@users.noreply.github.com>
2022-11-08 08:01:47 -0300

    calculadora em lua codigo fonte

---
+++ calculadora.lua
@@ -0,0 +1,21 @@
+print("digite o primeiro numeros que você quer calcular")
+x=io.read()
+print("digite o segundo numeros que você quer calcular")
+y=io.read()
+print("digite + para somar - para subitrair * para multiplicar / para dividir % para resto")
+z=io.read()
+if z == "+" then
+  print(x+y)
+  end
+   if z == "-" then
+  print(x-y)
+  end
+  if z=="*" then
+  print(x*y)
+  end
+  if z=="/" then
+  print(x/y)
+  end
+  if z=="%" then
+  print(x%y)
+  end
